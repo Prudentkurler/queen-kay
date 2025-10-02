10bdfbf256efc4bd4c1d3d3474b288d17a4eacd6
Prudentkurler <sarkodiekurler@gmail.com>
2025-10-02 14:31:11 +0000

    Home page modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import { Navbar } from '@/components/layout/Navbar';
 import { HeroSection } from '@/components/layout/HeroSection';
 import { SpotlightSection } from '@/components/layout/SpotlightSection';
 import { PreorderSection, InStockSection } from '@/components/product/Product-Grid';
 import { TestimonialsSection } from '@/components/layout/TestimonialsSection';
-import { Footer } from '@/components/layout/Footer';
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <main className="flex-1 lg:px-12">
-        <Navbar cartItemCount={3} />
-        <HeroSection />
-        <SpotlightSection />
-        <PreorderSection />
-        <InStockSection />
-        <TestimonialsSection />
-        <Footer />
-      </main>
-
-      
-    </div>
+    <>
+      <HeroSection />
+      <SpotlightSection />
+      <PreorderSection />
+      <InStockSection />
+      <TestimonialsSection />
+    </>
   );
 }
